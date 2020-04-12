11d76af53173846529d8b8a24fc3fd39db64058e
CroatiaParanoia <1369248927@qq.com>
2020-04-13 00:24:16 +0800

    refactor: 修改withStore文件内部导出函数名

--- src/middleware/withStore.tsx
+++ src/middleware/withStore.tsx
@@ -3,12 +3,12 @@ import { Container } from 'unstated-next';
 
 type TContainer<V, S> = Container<V, S>;
 
-const withProvider = <V, S>(Container: TContainer<V, S>, initialState?: any) => (
+const withStore = <V, S>(Container: TContainer<V, S>, initialState?: any) => (
   instance: React.ReactElement
 ) => {
   return <Container.Provider initialState={initialState}>{instance}</Container.Provider>;
 };
 
-export type WithProvider = typeof withProvider;
+export type WithStore = typeof withStore;
 
-export default withProvider;
+export default withStore;
